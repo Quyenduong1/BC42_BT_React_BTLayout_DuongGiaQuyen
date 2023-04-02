df4e3058071ef959ef796842cda6d9e56f638cb5
Quyen <dgiaquyen40@gmail.com>
2023-04-02 22:19:08 +0700

    Dang lam mentor cham bai tu tu '

---
+++ src/4_ProductForm/ProductForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './Form'
+import Header from './header'
+
+function ProductForm() {
+  return (
+    <div>
+        <Header/>
+        <Form/>
+    </div>
+  )
+}
+
+export default ProductForm
